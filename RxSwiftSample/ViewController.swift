eb84f8618619ae67f849c7d25bb6e6c4a7825e7d
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-03-10 02:06:51 +0900

    部品を作成

--- RxSwiftSample/ViewController.swift
+++ RxSwiftSample/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+struct Product {
+    let imageName:String
+    let title: String
+}
+
+struct ProductViewModel {
+    var items = PublishSubject<[Product]>()
+    
+    func fetchItem() {
+        let products = [
+            Product(imageName: "house", title: "Home"),
+            Product(imageName: "gear", title: "Settings"),
+            Product(imageName: "person.circle", title: "Profile"),
+            Product(imageName: "airplane", title: "Flights"),
+            Product(imageName: "bell", title: "Activity")
+        ]
+        
+        items.onNext(products)
+        items.onCompleted()
+    }
+}
+
 class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var viewModel = ProductViewModel()
+    
+    private var bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        bindTableData()
     }
 
 
